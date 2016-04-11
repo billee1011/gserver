e9bf61517c177de70122a6d2c447330f8f1e11ab
zhoucailiang <zhoucailiang@shijie99.com>
2016-04-11 18:39:23 +0800

    [Modify]命令行运行bug提交

--- DataServer/src/main/java/com/eboji/data/bootstrap/config/DatabaseConfig.java
+++ DataServer/src/main/java/com/eboji/data/bootstrap/config/DatabaseConfig.java
@@ -1,13 +1,14 @@
 package com.eboji.data.bootstrap.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 @ImportResource({"classpath:spring-mybatis.xml"})
-
+@EnableAspectJAutoProxy(proxyTargetClass=false)
 /**
  * 由于使用@MapperScan标签，无法通过指定sqlSessionFactoryBeanName指定特定的sqlSessionFactory
  * 故对于数据源和sqlSessionfactory，事务，mybatis自动扫描等暂时使用XML方式进行配置
